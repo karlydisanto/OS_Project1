bce765caf9dbcbc560e5b577c8fa7acc80de00cb
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-10-30 18:16:35 -0400

    Create SJF.cpp

---
+++ SJF.cpp
@@ -0,0 +1,14 @@
+#include<processor1>
+#include<processor2>
+#include<processor3>
+#include<processor4>
+#include<iostream>
+#include<queue>
+
+void SFJ(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<long long> processes) {
+  //sort the queue of processes from shortest time to most time
+  
+  //check if any of the processors are full
+  
+  //if not, the pop the next process in the queue onto the processor
+}
