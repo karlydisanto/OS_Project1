4d31c69c64aa38f2334c9502e679c4a6ebdc9452
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-10-30 18:08:37 -0400

    Update FIFO.cpp

--- FIFO.cpp
+++ FIFO.cpp
@@ -1,1 +1,15 @@
 
+//Passing processor objects to the different algorithm
+#include<processor1>
+#include<processor2>
+#include<processor3>
+#include<processor4>
+#include<iostream>
+#include<queue>
+
+//passing memory and speed requirements based on different scenarios
+void FIFO(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<long long> processes){
+  //check if any of the processor are full
+  //if they are not full then pop the next process in the queue and add it to the next available processor
+  
+}
