5dfc9818263d658e52d8c2ef52bedcfe161e21ab
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-10-30 18:50:29 -0400

    Update FIFO.cpp

--- FIFO.cpp
+++ FIFO.cpp
@@ -9,7 +9,13 @@
 
 //passing memory and speed requirements based on different scenarios
 void FIFO(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<long long> processes){
-  //check if any of the processor are full
+  long long processesCompleted = 0;
+  while(processesCompleted != processes.size()){
+//check if any of the processors are not empty (full)
+    if(!processor1.isEmpty() || !processor2.isEmpty() || !processor3.isEmpty() || !processor4.isEmpty()) {
+       
+    }
+  }
   //if they are not full then pop the next process in the queue and add it to the next available processor
   
 }
