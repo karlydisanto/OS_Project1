f71274b93c3eb86fc557c7cda7fb9bd8d96302d7
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-10-30 16:57:45 -0400

    Update project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -33,51 +33,64 @@ void menu ()
         cin >> scenario;
   
   //user can enter the seed value to generate random service times and memory requirements
+        queue<long long> p;  
         long long seedValue;
         cout << "Enter a seed value"
         cin >> seedValue;
+  //64 bit seed
+        mt19937_64 seed(seedValue);
+  //the random values that the service time and memory can be
+        uni_cost_distro<long long> serv(1000000, 1000000000000);
+        uni_cost_distro<int> mem(1,8);
+  //creates the 40 processes with random service time and mem requirements based on the seed provided and then puts them into a queue
+        for (int i = 0; i <= 40; i++){
+          long long servTime =serv(seed);
+          int memSize = mem(seed); 
+          cout << servTime << memSize << "\n";
+          
+          
 
- //generates based on scenario entered
-   if (scenario == 1)
-   {
-     //FIFO using scenario 1
-       printf("Hello World1");
-   }
-   else if (scenario == 2)
-   {
-     //SJF using scenario 1
-       printf("Hello World2");
-   }
-   else if (scenario == 3)
-   {
-     //FIFO using scenario 2
-       printf("Hello World3");
-   }
-   else if (scenario == 4)
-    {
-     //SJF using scenario 2
-       printf("Hello World4");
-    }
-   else if (scenario == 5)
-    {
-     //FIFO using scenario 3
-       printf("Hello World5");
-    }
-   else if (scenario == 6)
-    {
-     //SJF using scenario 3
-       printf("Hello World6");
-    }
-   else if (scenario == 7)
-    {
-     //FIFO using scenario 4
-       printf("Hello World7");
-    }
-   else if (scenario == 8)
-    {
-     //SJF using scenario 4
-      printf("Hello World8");
-    }
-  }
+     //generates based on scenario entered
+       if (scenario == 1)
+       {
+         //FIFO using scenario 1
+           printf("Hello World1");
+       }
+       else if (scenario == 2)
+       {
+         //SJF using scenario 1
+           printf("Hello World2");
+       }
+       else if (scenario == 3)
+       {
+         //FIFO using scenario 2
+           printf("Hello World3");
+       }
+       else if (scenario == 4)
+        {
+         //SJF using scenario 2
+           printf("Hello World4");
+        }
+       else if (scenario == 5)
+        {
+         //FIFO using scenario 3
+           printf("Hello World5");
+        }
+       else if (scenario == 6)
+        {
+         //SJF using scenario 3
+           printf("Hello World6");
+        }
+       else if (scenario == 7)
+        {
+         //FIFO using scenario 4
+           printf("Hello World7");
+        }
+       else if (scenario == 8)
+        {
+         //SJF using scenario 4
+          printf("Hello World8");
+        }
+      }
 
 
