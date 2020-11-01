767e6cea936e37b43bbb470824010572a7658981
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-01 18:28:52 -0500

    Update SJF.h

--- SJF.h
+++ SJF.h
@@ -3,4 +3,6 @@
 
 using namespace std;
 
-void SFJ
+void SJF(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<int> p, queue<int> m, queue<long long> sT, long int turnaround, long int waiting) {};
+
+#endif
