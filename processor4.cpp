6841b2c85235090856829ba50165624c287c7d7f
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-01 15:03:06 -0500

    Update processor4.cpp

--- processor4.cpp
+++ processor4.cpp
@@ -1,1 +1,8 @@
+#include<unistd.h>
+#include<iostrem>
+#include<queue>
 
+void processor4(int speed, int memory, int currProcess){
+   
+    cout << " Process ID: " << processID << "\n";
+    return speed, memory, currProc
