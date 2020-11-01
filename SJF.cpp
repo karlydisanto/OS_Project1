f02fd8f74f375d02efc4e51049d31f7250a9e921
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-01 17:12:09 -0500

    Update SJF.cpp

--- SJF.cpp
+++ SJF.cpp
@@ -1,11 +1,12 @@
-#include<processor1>
-#include<processor2>
-#include<processor3>
-#include<processor4>
+//#include<processor1>
+//#include<processor2>
+//#include<processor3>
+//#include<processor4>
 #include<iostream>
 #include<queue>
+#include "processor.h"
 
-void SFJ(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<long long> processes) {
+void SJF(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<int> p, queue<int> m, queue<long long> sT, long int turnaround, long int waiting) {
   //sort the queue of processes from shortest time to most time
   
   //check if any of the processors are full
