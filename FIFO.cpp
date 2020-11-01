f0e917224e4257aadd536b2c2bb9be0debe63f95
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-01 14:56:01 -0500

    Update FIFO.cpp

--- FIFO.cpp
+++ FIFO.cpp
@@ -16,31 +16,71 @@ boolean P4 = false;
 
 
 //passing memory and speed requirements based on different scenarios
-void FIFO(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<long long> processes){
+void FIFO(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<int> p, queue<int> m, queue<long long> sT){
   long long processesCompleted = 0;
-  while(processesCompleted != processes.size()){
-//check if any of the processors are full
-    if(!P1 || !P2 || !P3 || !P4) {
+  while(!p.empty())){
+//check if all of the processors are full
+    if(!P1 && !P2 && !P3 && !P4) {
        //DO SOMETHING
     }
     else {
 //if any of the processors are empty pop the next process off the queue into that processor
         if(P1) {
 //create a temp which holds the current process at the front
-           long long temp = processes.front();
+           int pNumber = p.front();
 //get the memory size to check if it fits in the processor
-           int memOfProcess = temp.servTime;
-          
-           processes.pop();
+           int memSize = m.front();
+           long long servTime = sT.front();
+//compare the mem size of the process and the processor (to see if the process fits in the processor)
+           if(memSize <= mem1) {
+            p.pop();
+            m.pop();
+            sT.pop();
+            P1 = true; 
+             
+           }
         }
         else if(P2) {
-           processes.pop(); 
+ //create a temp which holds the current process at the front
+           int pNumber = p.front();
+//get the memory size to check if it fits in the processor
+           int memSize = m.front();
+           long long servTime = sT.front();
+//compare the mem size of the process and the processor (to see if the process fits in the processor)
+           if(memSize <= mem1) {
+            p.pop();
+            m.pop();
+            sT.pop();
+            P2 = true;
+           } 
         }
         else if(P3) {
-           processes.pop(); 
+//create a temp which holds the current process at the front
+           int pNumber = p.front();
+//get the memory size to check if it fits in the processor
+           int memSize = m.front();
+           long long servTime = sT.front();
+//compare the mem size of the process and the processor (to see if the process fits in the processor)
+           if(memSize <= mem1) {
+            p.pop();
+            m.pop();
+            sT.pop();
+            P3 = true;
+           } 
         }
         else if(P4) {
-           processes.pop(); 
+//create a temp which holds the current process at the front
+           int pNumber = p.front();
+//get the memory size to check if it fits in the processor
+           int memSize = m.front();
+           long long servTime = sT.front();
+//compare the mem size of the process and the processor (to see if the process fits in the processor)
+           if(memSize <= mem1) {
+            p.pop();
+            m.pop();
+            sT.pop();
+            P4 = true;
+           } 
         }  
     }
   }
