cfe792f760334ce4a4f556efd23871103a0ec180
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-01 18:03:40 -0500

    Update FIFO.h

--- FIFO.h
+++ FIFO.h
@@ -1,5 +1,6 @@
 #ifndef FIFO_H
 #define FIFO_H
+using namespace std;
 
 void FIFO(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<int> p, queue<int> m, queue<long long> sT, long int turnaround, long int waiting) {};
 
