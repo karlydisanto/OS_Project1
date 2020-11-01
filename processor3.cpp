e3a06277ab698a35fe787e543648cbbb9cf4908f
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-01 15:02:17 -0500

    Update processor3.cpp

--- processor3.cpp
+++ processor3.cpp
@@ -1,1 +1,8 @@
+#include<unistd.h>
+#include<iostrem>
+#include<queue>
 
+void processor3(int speed, int memory, int currProcess){
+   
+    cout << " Process ID: " << processID << "\n";
+    return speed, memory, currProcess
