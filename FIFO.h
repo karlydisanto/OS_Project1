c4698f94d1e24db6b60a7814ecf2e562d86d00f1
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-01 17:56:00 -0500

    Update FIFO.h

--- FIFO.h
+++ FIFO.h
@@ -1,7 +1,7 @@
 #ifndef FIFO_H
 #define FIFO_H
 
-void FIFO
+void FIFO(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<int> p, queue<int> m, queue<long long> sT, long int turnaround, long int waiting) {};
 
 
 #endif
