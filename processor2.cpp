88b0c1999ea9bdaff699d2bcbca0acaa509df9b2
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-01 15:01:03 -0500

    Update processor2.cpp

--- processor2.cpp
+++ processor2.cpp
@@ -1,1 +1,9 @@
 
+#include<unistd.h>
+#include<iostrem>
+#include<queue>
+
+void processor1(int speed, int memory, int currProcess){
+   
+    cout << " Process ID: " << processID << "\n";
+    return speed, memory, currProcess;
