fc577626b31fbbb30f5faff7348be3b58ec9558b
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-01 20:03:05 -0500

    Update SJF.cpp

--- SJF.cpp
+++ SJF.cpp
@@ -10,20 +10,6 @@
 
 using namespace std;
 
-void SJF(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<int> p, queue<int> m, queue<long long> sT, long int turnaround, long int waiting) {
-    int min_val; 
-    for (int i = 0; i < 40; i++) 
-    { 
-        int currProc = p.front(); 
-        int currsT = sT.front();
-        int currMem = m.front();
-        p.pop();
-        sT.pop();
-        m.pop();
-        if (i != min_index) 
-            sT.push(curr); 
-        else
-            min_val = curr; 
-    } 
-    sT.push(min_val);
+void SJF(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, process proc[], long int turnaround, long int waiting) {
+    sort(proc, proc + 39);
 }
