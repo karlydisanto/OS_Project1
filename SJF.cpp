3de27a508348580f78709bb7506d32b036598c36
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-01 20:43:08 -0500

    Update SJF.cpp

--- SJF.cpp
+++ SJF.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-void SJF(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, process[] proc, int[] turnaround, long[] waiting) {
+void SJF(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, process proc[], int turnaround[], long waiting[]) {
 //loop through the number of processes
     for(i=0;i<40;i++)
     {
