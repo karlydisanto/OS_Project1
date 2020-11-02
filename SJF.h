f97a3cc97a61bdb1562fcfce9b01a12f04a01fed
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-01 20:39:21 -0500

    Update SJF.h

--- SJF.h
+++ SJF.h
@@ -3,6 +3,6 @@
 
 using namespace std;
 
-void SJF(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<int> p, queue<int> m, queue<long long> sT, long int turnaround, long int waiting) {};
+void SJF(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, process[] proc, int[] turnaround, long[] waiting){};
 
 #endif
