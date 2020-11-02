ce8347c545502fadbea11bdea53fb5defd118474
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2020-11-01 21:19:17 -0500

    Create SJF_2.h

---
+++ SJF_2.h
@@ -0,0 +1,6 @@
+#ifndef SJF_2_H
+#define SJF_2_H
+
+void SJF_2(int mem1, int mem2, int mem3, int mem4, int speed1, int speed2, int speed3, int speed4, queue<int> p, queue<int> m, queue<long long> sT, long turnaround, long waiting) {};
+
+#endif
